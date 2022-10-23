7cbe315727fb1aeb4b4612d427407e239c53f410
Ross Higa <rhiga1@my.wgu.edu>
2022-10-23 06:11:54 -1000

    Inital Load from Cloudflare Worker Template
    this is the default template they are pushing

---
+++ CF-Worker-lowcode.js
@@ -0,0 +1,34 @@
+export default {
+  async fetch(request, env) {
+    return await handleRequest(request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  }
+}
+
+/**
+ * Many more examples available at:
+ *   https://developers.cloudflare.com/workers/examples
+ * @param {Request} request
+ * @returns {Promise<Response>}
+ */
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.startsWith("/api")) {
+    return new Response(JSON.stringify({ pathname }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (pathname.startsWith("/status")) {
+    const httpStatusCode = Number(pathname.split("/")[2]);
+
+    return Number.isInteger(httpStatusCode)
+      ? fetch("https://http.cat/" + httpStatusCode)
+      : new Response("That's not a valid HTTP status code.");
+  }
+
+
+  return fetch("https://welcome.developers.workers.dev");
+}
