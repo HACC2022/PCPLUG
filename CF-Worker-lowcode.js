c452a63ea50e107412498c0b5a842fccb2b9acee
Ross Higa <rhiga1@my.wgu.edu>
2022-10-30 13:55:30 -1000

    Worker Cleanup
    Remove extra code and add comments for each section and line

--- CF-Worker-lowcode.js
+++ CF-Worker-lowcode.js
@@ -1,99 +1,91 @@
 export default {
-  async fetch(request, env) {
-     console.log(request)
-    return await handleRequest(request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  }
+	async fetch(request, env) {
+		return await handleRequest(request).catch(
+			(err) => new Response(err.stack, {
+				status: 500
+			})
+		)
+	}
 }
-const budiApiGet = "https://edit.hacc.ga/api/public/v1/tables/ta_6bc0c2af92e94a6cb04c0a6d8ea9ee38/rows/search"
-const budiApiSet = "https://edit.hacc.ga/api/public/v1/tables/ta_62932153e9b141cf970ca23e21721c73/rows"
+const bearerToken = "Bearer IamASecureBearToken" // PLEASE CHANGE FOR SECURITY, must match in Budibase
+const budiApiGet = "https://edit.hacc.ga/api/public/v1/tables/ta_6bc0c2af92e94a6cb04c0a6d8ea9ee38/rows/search" // Find row see https://docs.budibase.com/reference/post_tables-tableid-rows-search
+const budiApiSet = "https://edit.hacc.ga/api/public/v1/tables/ta_62932153e9b141cf970ca23e21721c73/rows" // Create Tracking row, see https://docs.budibase.com/reference/post_tables-tableid-rows
+const budiApiKey = { // API keys from budibase https://docs.budibase.com/docs/public-api
+	'content-type': "application/json",
+	'accept': "application/json",
+	'x-budibase-app-id': "app_731acc3a5d304eca904f5e326e69a669", // Get from Budibase without "_dev"
+	'x-budibase-api-key': "9b546f8e88a2e9ee2277ddf84500b0f5-55ca69dd4c128c122f34492849be2e5ac15f7d70e39fee08bc702361000d9473e9b0fbc81df63463"
+}
 
-/**
- * Many more examples available at:
- *   https://developers.cloudflare.com/workers/examples
- * @param {Request} request
- * @returns {Promise<Response>}
- */
 async function handleRequest(request) {
-  const { pathname } = new URL(request.url);
-  const bearerToken = "Bearer IamASecureBearToken"
-
-if(request.headers.get("Authorization") === bearerToken){
-  if (pathname.startsWith("/api")) {
-    return new Response(JSON.stringify({ pathname }), {
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-
-  if (pathname.startsWith("/status")) {
-    const httpStatusCode = Number(pathname.split("/")[2]);
-
-    return Number.isInteger(httpStatusCode)
-      ? fetch("https://http.cat/" + httpStatusCode)
-      : new Response("That's not a valid HTTP status code.");
-  }
-
-  if (pathname.startsWith("/check")) {
-
-    console.log(request.headers.get("url"));
-
-    const url = atob(request.headers.get("url")); //Get base64 portion of URL from header "url"
-    console.log(url);
-
-    let req = new Request(url, { Method: 'HEAD', signal: AbortSignal.timeout(10000),}); // https://developers.cloudflare.com/workers/examples/modify-request-property/
-    let result = "OK" // A result starts a "OK" and we check against all errors
-    try{ let z = await fetch(req); 
-        console.log(z);
-        if(z.redirected){result = "Redirected"}
-        else if(Boolean(z.ok) === false ){result = "Error"; z.url = url};
-       return new Response(JSON.stringify({res: result, endurl : z.url  }, {headers: { "Content-Type": "application/json" },}))    ;
-
-    } // https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/timeout
-    catch(err){ return new Response(JSON.stringify({res:"TCP" }, {headers: { "Content-Type": "application/json" },})) };
-  }
-}
-
-const body = {  query : {        "string": {"shortcode": pathname.substring(1)},
-        "equal": {
-            "Approved": "true",
-            "Status": "OK"
-  }},
-  limit: 1
-};
-let init = {
-    body: JSON.stringify(body),
-    method: 'POST',
-    headers: {
-          'accept': "application/json",
-    'x-budibase-app-id': "app_731acc3a5d304eca904f5e326e69a669",
-    'content-type': "application/json",
-    'x-budibase-api-key': "9b546f8e88a2e9ee2277ddf84500b0f5-55ca69dd4c128c122f34492849be2e5ac15f7d70e39fee08bc702361000d9473e9b0fbc81df63463"
-}
-  };
-  const response = await fetch(budiApiGet, init);
-  let x = await response.json();
-  if(x.data.length === 0 )
-  {return new Response('Not found, invalid shortcode') }
-  else{
-
-    init = {
-    body: JSON.stringify({shortcode: pathname.substring(1)}),
-    method: 'POST',
-    headers: {
-    'accept': "application/json",
-    'x-budibase-app-id': "app_731acc3a5d304eca904f5e326e69a669",
-    'content-type': "application/json",
-    'x-budibase-api-key': "9b546f8e88a2e9ee2277ddf84500b0f5-55ca69dd4c128c122f34492849be2e5ac15f7d70e39fee08bc702361000d9473e9b0fbc81df63463"
-    }
-
-}
-
- fetch(budiApiSet, init);
-
-}
-    
-  
-
-    return Response.redirect(x.data[0].url, 307);
+	const {		pathname	} = new URL(request.url); // Get the requested URL
+	if (request.headers.get("Authorization") === bearerToken) { // Non-public API functions need bearer token
+		if (pathname.startsWith("/check")) { // API call to "/check/ endpoint"
+			const url = atob(request.headers.get("url")); //Get base64 portion of URL from header "url"
+			let req = new Request(url, {
+				Method: 'HEAD',
+				signal: AbortSignal.timeout(10000),
+			}); // https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/timeout
+			let result = "OK" // A result starts a "OK" and we check against all errors
+			try {
+				let z = await fetch(req); // https://developers.cloudflare.com/workers/examples/modify-request-property/
+				if (z.redirected) {
+					result = "Redirected"
+				} // Check for redirected websites
+				else if (Boolean(z.ok) === false) {
+					result = "Error";
+					z.url = url
+				}; // Check for 200 or other OK code
+				return new Response(JSON.stringify({
+					res: result,
+					endurl: z.url
+				}, {
+					headers: {
+						"Content-Type": "application/json"
+					},
+				})); // Return JSON to Budibase
+			} catch (err) {
+				return new Response(JSON.stringify({
+					res: "TCP"
+				}, {
+					headers: {
+						"Content-Type": "application/json"
+					},
+				}))
+			}; //Could not complete Fetch, return TCP error
+		}
+	}// End nonpublic, no bearer token 
+	// Start of public API section, build query to Budibase must be Approved and OK
+	let shortcodeQuery = {
+		body: JSON.stringify({
+			query: {
+				"string": {
+					"shortcode": pathname.substring(1) // Get the short code from the path
+				},
+				"equal": {
+					"Approved": "true", 
+					"Status": "OK"
+				}
+			},
+			limit: 1
+		})  ,
+		method: 'POST', // POST for Budibase API, see https://docs.budibase.com/reference/post_tables-tableid-rows-search
+		headers: budiApiKey // API keys from budibase https://docs.budibase.com/docs/public-api
+	};
+	const getURL = await fetch(budiApiGet, shortcodeQuery); // Fetch url from Budibase
+	let longUrl = await getURL.json();
+	if (longUrl.data.length === 0) // No shortcode in Budibase for that URL
+	{
+		return new Response('Not found, invalid shortcode')
+	} else { // Else shortcode found, send ping back to Tracking Table in Budibase
+		init = {
+			body: JSON.stringify({
+				shortcode: pathname.substring(1)
+			}),
+			method: 'POST',
+			headers: budiApiKey
+		}
+		fetch(budiApiSet, init); //Send hit to Budibase
+	}
+	return Response.redirect(longUrl.data[0].url, 307); // Return the redirect to the client
 }
